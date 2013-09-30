4f3a81cd681c0115e3b1a7bb3862a1467d9429d4
Hackbright Student <user@chromebox-002.(none)>
2013-09-30 15:40:28 -0700

    Pretty much working

--- wordcount.py
+++ wordcount.py
@@ -3,12 +3,27 @@ from sys import argv
 script, input_file = argv
 
 f = open(input_file)
-text_file = f.read()
+text_file = f.read().lower()
 
-# split by spaces
-lines = text_file.splitlines()
-words = " ".join(lines)
-words = words.split(" ")
+def normalize(string):
+    string = string.lower()
+    for i in string:
+        if not i.isalpha() and not i.isspace():
+            string = string.replace(i, "")
+    return string
+
+def sort_by_freq_and_alpha(dict):
+    new_dict = {}
+    for key,val in dict.iteritems():
+        if val not in new_dict:
+            new_dict[val] = [key]
+        else:
+            new_dict[val].append(key)
+            new_dict[val].sort()
+    return new_dict
+
+
+words = normalize(text_file).split()
 
 # for each word, make it a key in the dictionary and give value 1
 word_count = {}
@@ -18,10 +33,10 @@ for word in words:
     else:
         word_count[word] = 1
 
-# print all keys and values
-for key,value in word_count.items():
-    print key,value
+sorted_word_count = sort_by_freq_and_alpha(word_count)
 
-
+for key,val in sorted_word_count.iteritems():
+    print key,val
+        
 
 f.close()
